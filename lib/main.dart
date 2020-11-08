cdafb57661cfb94587b286dd1f46b647cb2b41dc
hanna2100 <hanna0497@naver.com>
2020-11-08 12:04:34 +0900

    컨테이너 위젯 사용방법

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lime,
-        appBar: AppBar(
-          title: Text('I am rich'),
-          backgroundColor: Colors.lightGreen[900],
-        ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
+        backgroundColor: Colors.amber,
+        body: SafeArea(
+          child: Container(
+            width: 100.0,
+            height: 100.0,
+            margin: EdgeInsets.only(left: 40.0),
+            padding: EdgeInsets.all(30.0),
+            color: Colors.white,
+            child: Text('Hello'),
           ),
         ),
-      )
-    ),
-  );
+      ),
+    );
+  }
 }
