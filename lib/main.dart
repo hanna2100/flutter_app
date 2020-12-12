eaa07e72773f7b53fb14175549af25733ed75a3e
hanna2100 <hanna0497@naver.com>
2020-12-12 17:46:39 +0900

    card

--- lib/main.dart
+++ lib/main.dart
@@ -13,30 +13,79 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.amber,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 30.0,
-                height: 100.0,
-                color: Colors.white,
-                child: Text('컨테이너1'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/hwasa.jpg'),
+              ),
+              Text(
+                'Hwa_sa',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'NotoSansCJKR',
+                  color: Colors.amber.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               SizedBox(
-                width: 20.0,
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                color: Colors.amber.shade100,
+                ),
               ),
-              Container(
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.amber,
+                    ),
+                    title: Text('+82 010 1234 5678',
+                      style: TextStyle(
+                        color:Colors.amber.shade900,
+                        fontFamily: 'NotoSansCJKR',
+                        fontSize: 18.0,
+                      ),
+                    )
+                )
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.amber,
+                  ),
+                  title: Text(
+                    'hwasa@email.com',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.amber.shade900,
+                      fontFamily:'NotoSansCJKR'),
+                  ),
+                )
+              ),
+              SizedBox(
                 height: 100.0,
-                color: Colors.blue,
-                child: Text('컨테이너2')
-              ),
-              Container(
-                  height: 100.0,
-                  color: Colors.red,
-                  child: Text('컨테이너3')
-              ),
+              )
             ],
-          ),
+          )
         ),
       ),
     );
