4b09d699938ac8664344cbbb00bb75cd670ae84c
hanna2100 <hanna0497@naver.com>
2021-02-20 21:25:59 +0900

    chapter10 완성

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
@@ -25,6 +31,7 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +68,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
@@ -79,19 +86,56 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: quizBrain.scoreKeeper,
+        )
       ],
     );
   }
+
+  void checkAnswer(bool userPick) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+    quizBrain.addScorekeeper(correctAnswer);
+    setState(() {
+      quizBrain.nextQuestion(alertFinished);
+    });
+  }
+
+
+  void alertFinished() {
+    Alert(
+      context: context,
+      type: AlertType.info,
+      title: "Finished!",
+      desc: "You've reached the end of the quiz.",
+      closeFunction: () {
+        setState(() {
+          Navigator.pop(context);
+          quizBrain.resetQuestion();
+        });
+      },
+      closeIcon: Icon(Icons.cancel),
+      buttons: [
+        DialogButton(
+          child: Text(
+            "CANCEL",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {
+            setState(() {
+              Navigator.pop(context);
+              quizBrain.resetQuestion();
+            });
+          },
+          width: 120,
+        )
+      ],
+    ).show();
+  }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
---
+++ quizzler/lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  //생성자
+  Question(this.questionText, this.questionAnswer);
+  // Question(String q, bool a) {
+  //   questionText = q;
+  //   questionAnswer = a;
+  // }
+}
